6ad0986ccad28c49d1737e51563037a3b3a398cb
Rajakumardev <rajakumarofficial@gmail.com>
2021-04-01 10:01:44 +0530

    webhoot and trans log added

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { default: axios } = require('axios');
+const transactionLog = require('./log/transactionlog');
 
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//root
 app.get('/', (req, res) => {
     res.json({
         "test": "it works"
     });
-})
-app.listen(3000, (err) => {
+});
+
+//verify transaction with reference id
+app.get('/verifypayment/:reference', (req, res) => {
+    const url = `https://api.paystack.co/transaction/verify/${req.params.reference}`;
+    axios.get(url, {
+        headers: {
+            'Authorization': 'Bearer sk_test_eb036fb6acd6d388694add407ed6da1d7202b946'
+        }
+    }).then((response) => {
+        if (response.data.status) {
+            res.json({
+                status: 'success',
+                reference: req.params.reference
+            });
+        } else {
+            res.json({
+                status: 'failed',
+                reference: req.params.reference
+            });
+        }
+    }).catch((error) => {
+        res.json({
+            status: failed
+        });
+    });
+});
+
+
+//webhook
+app.post('/paystackwebhook', (req, res) => {
+    transactionLog.push(req.body);
+    res.send(200);
+});
+
+app.get('/transactionlog', (req, res) => {
+    res.json({ transactionLog });
+});
+
+//server start
+app.listen(8000, (err) => {
     if (err) throw err;
     console.log('server started');
-})
+});
---
+++ log/transactionlog.js
@@ -0,0 +1,4 @@
+
+let transactionLog = [];
+
+module.exports = transactionLog;
